b044ea50b36c3e345ea201182884ec0c0d3f193a
Gerardo R <gromero@folcode.com>
2022-03-02 19:24:06 -0300

    fixes data types Product.interface

--- src/interfaces/productos.interface.ts
+++ src/interfaces/productos.interface.ts
@@ -3,8 +3,8 @@ import { Document } from 'mongoose'
 export default interface IProduct extends Document {
   _id: string;
   nombre: string;
-  precio: string;
-  stock: string;
+  precio: number;
+  stock: number;
   estado: string;
   createdAt: Date;
   updatedAt: Date;
