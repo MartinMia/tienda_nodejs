60f8ed68189c154c647eb50504ed5d37481e8dcb
Gerardo R <gromero@folcode.com>
2022-03-03 20:54:02 -0300

    added comment to connection

--- database/connection.ts
+++ database/connection.ts
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 export const configureDatabase = async () => {
   mongoose.connect(process.env.MONGODB_URI || '', {})
-    .then(() => { console.log('🟢 The database is connected.'); mongoose.set('debug', true); })
+    .then(() => { 
+      console.log('🟢 The database is connected.'); 
+      // si no quieren ver las funciones que se ejecutan en la DB,
+      // comenten la linea 9
+      mongoose.set('debug', true); 
+    })
     .catch((error: Error) => { console.error(`🔴 Unable to connect to the database: ${error}.`); });
 };
